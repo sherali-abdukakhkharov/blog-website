fda2639d5dad1f8cb2d4fda9d8bd6d87c6f8e0b1
unknown <sherali.abdukakhkharov@gmail.com>
2021-01-31 01:38:53 +0500

    Database connec

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect('mongodb://localhost:27017/blogDB', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb+srv://admin-sherali:test123@todolist.govc7.mongodb.net/blogDB', {useNewUrlParser: true, useUnifiedTopology: true});
 
 const postSchema = {
   title: String,
